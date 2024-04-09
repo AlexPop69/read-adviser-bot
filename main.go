74ff8ec20ee8fa5a2bd432e405a58de369d009e8
Popov Aleksandr <lex.popov.1992@gmail.com>
2024-04-09 14:32:54 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+
+	t := mustToken()
+	// tgClient = telegram.New(token)
+
+	// fetcher = fetcher.New(tgClient)
+
+	// processor = processor.New(tgClient)
+
+	// consumer.Start(fetcher, processor)
+}
+
+// функция для получения токена
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
