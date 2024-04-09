ab06ef6e1d35ebd70ffeda8c2e2e7fad9c36bb66
Popov Aleksandr <lex.popov.1992@gmail.com>
2024-04-09 19:07:46 +0800

    Add storage for pages

---
+++ storage/storage.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"crypto/sha1"
+	"io"
+
+	"read-adviser-bot/lib/e"
+)
+
+type Storage interface {
+	Save(p *Page) error
+	PickRandom(userName string) (*Page, error)
+	Remove(p *Page) error
+	IsExist(p *Page) (bool, error)
+}
+
+// страница на которую ведет ссылка, которую мы скинули боту
+type Page struct {
+	URL      string
+	UserName string
+}
+
+func (p Page) Hash() (string, error) {
+	h := sha1.New()
+	if _, err := io.WriteString(h, p.URL); err != nil {
+		return "", e.Wrap("can't calculate hash", err)
+	}
+
+	if _, err := io.WriteString(h, p.UserName); err != nil {
+		return "", e.Wrap("can't calculate hash", err)
+	}
+
+	return string(h.Sum(nil)), nil
+}
