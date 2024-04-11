abbbf7f9f5b415a250327a71ab6a8d987746a036
Popov Aleksandr <lex.popov.1992@gmail.com>
2024-04-11 10:57:10 +0800

    add method Fetch

---
+++ events/telegram/telegram.go
@@ -0,0 +1,80 @@
+package telegram
+
+import (
+	"read-adviser-bot/clients/telegram"
+	"read-adviser-bot/events"
+	"read-adviser-bot/lib/e"
+	"read-adviser-bot/storage"
+)
+
+type Processor struct {
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
+}
+
+type Meta struct {
+	ChatID   int
+	Username string
+}
+
+func New(client *telegram.Client, storage *storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: *storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	updates, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can't get events", err)
+	}
+
+	if len(updates) == 0 {
+		return nil, nil
+	}
+	result := make([]events.Event, 0, len(updates))
+
+	for _, upd := range updates {
+		result = append(result, event(upd))
+	}
+
+	p.offset = updates[len(updates)-1].ID + 1
+
+	return result, nil
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatID:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
+
+	return res
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+
+	return upd.Message.Text
+}
